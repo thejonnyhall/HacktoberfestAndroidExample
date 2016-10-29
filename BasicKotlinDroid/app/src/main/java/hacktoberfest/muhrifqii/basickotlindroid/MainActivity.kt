f23dbb75cb56993c7f942784f8a2abfcc06cbb39
muhrifqii <muh_rif@live.com>
2016-10-29 23:39:25 +0700

    add simple kotlin syntax

--- BasicKotlinDroid/app/src/main/java/hacktoberfest/muhrifqii/basickotlindroid/MainActivity.kt
+++ BasicKotlinDroid/app/src/main/java/hacktoberfest/muhrifqii/basickotlindroid/MainActivity.kt
@@ -2,11 +2,13 @@ package hacktoberfest.muhrifqii.basickotlindroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_kotlin.setOnClickListener { tv_hello.text = "Hello Kotlin!" }
     }
 }
